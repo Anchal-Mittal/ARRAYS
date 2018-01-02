bcc0f2cf98f8dd507235335b9757e159293bc476
ANCHAL MITTAL <32476930+Anchal-Mittal@users.noreply.github.com>
2018-01-02 19:24:28 +0200

    find the double repeated when all elements are repeated thrice

---
+++ findDuplicate.cpp
@@ -0,0 +1,23 @@
+//=========================SORT THE ARRAY IN WAVING FORM================================
+
+#include<iostream>
+using namespace std;
+
+void sortWave(int Array[10],int size){
+
+
+
+return;
+}
+
+int main(){
+	int array[10];
+	int size;
+	cout << "ENETR THE SIZE OF THE ARRAY "<< endl;
+	cin  >> size;
+	cout << "ENTER THE ELEMENTS OF THE ARRAAY "<< endl;
+	for(int i=0;i<size;i++)
+		cin >> array[i];
+	sortWave(array,size);
+return 0;
+}
